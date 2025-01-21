fdfb44ef8b4fb846338c5d52299497c17b620ab9
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-01-21 23:44:32 +0000

    hello ruby

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+# now people can just ruby-sandbox main % bundle install to run this file
+# and donwload all required gem packages
+
+gem "activesupport"
+gem "pry-byebug"
+gem "better_errors"
+gem "ruby-openai"
---
+++ hello.rb
@@ -0,0 +1,11 @@
+# set of very handy packages to have (remember to gem install first OR )
+require "active_support/all"
+require "pry-byebug"
+require "better_errors"
+
+x = gets.chomp # adding the chomp, we won't get a weird \n thing
+
+pp x
+
+# at terminal, typing irb you'll be able to test one line at a time
+# exit by typing exit
